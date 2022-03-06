67a9ec8f9fbdeb1a9051c46c04b929d4f73d5ce8
MarketaH <habranova.marketa@gmail.com>
2022-03-06 22:11:21 +0100

    Renaming variable

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ users = {
     'liz': 'pass123'
 }
 
-max_num_of_texts = len(TEXTS)
+number_of_texts = len(TEXTS)
 
 # -------------------------------
 
@@ -26,18 +26,18 @@ if not users.get(username) == password:
 else:
     print(separator)
     print('Welcome to the app', username.title())
-    print(f'We have {max_num_of_texts} texts to be analyzed.')
+    print(f'We have {number_of_texts} texts to be analyzed.')
     print(separator)
-    user_choice = input(f'Enter a number between 1 and {max_num_of_texts} to select: ')
+    user_choice = input(f'Enter a number between 1 and {number_of_texts} to select: ')
 
     if not user_choice.isnumeric():
 
         print('entered value is not a number, terminating the program..')
         quit()
 
-    elif int(user_choice) <= 0 or int(user_choice) > max_num_of_texts:
+    elif int(user_choice) <= 0 or int(user_choice) > number_of_texts:
 
-        print(f'entered number is not between 1 and {max_num_of_texts}, terminating the program..')
+        print(f'entered number is not between 1 and {number_of_texts}, terminating the program..')
         quit()
 
     else:
